b7beb1db9c676854eb7d7625e6296a5c049f6452
gabriela-data <gabiecanelaa@gmail.com>
2025-02-25 17:15:13 -0300

    conta-terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome do cliente: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o número da conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite a agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        //imprimindo os dados obtidos pelo usuário
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numero + " e seu saldo de " + saldo + " já está disponível para saque." );
     }
 }
